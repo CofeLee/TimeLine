cedf737e21a02fb14eb7d478b7f460fa38eff570
李廷农 <ltn1219@163.com>
2019-09-11 23:24:14 +0800

    Test2

--- app/src/main/java/com/cofe/timeline/MainActivity.java
+++ app/src/main/java/com/cofe/timeline/MainActivity.java
@@ -3,6 +3,7 @@ package com.cofe.timeline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.i("cofe", "HAHA!");
     }
 }
