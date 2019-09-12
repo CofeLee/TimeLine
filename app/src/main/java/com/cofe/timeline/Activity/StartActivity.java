47e5457af98c56a3c5a29571cbb888e8d605b2a9
李廷农 <litn@sdc.icbc.com.cn>
2019-09-12 17:03:45 +0800

    123

--- app/src/main/java/com/cofe/timeline/Activity/StartActivity.java
+++ app/src/main/java/com/cofe/timeline/Activity/StartActivity.java
@@ -1,8 +1,6 @@
 package com.cofe.timeline.Activity;
 
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cofe.timeline.R;
@@ -10,7 +8,7 @@ import com.cofe.timeline.R;
 public class StartActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
