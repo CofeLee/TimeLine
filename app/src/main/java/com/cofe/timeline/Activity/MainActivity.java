f48e75cef1aaf93d90f29840b409591c110de649
李廷农 <litn@sdc.icbc.com.cn>
2019-09-12 15:35:01 +0800

    第一

--- app/src/main/java/com/cofe/timeline/Activity/MainActivity.java
+++ app/src/main/java/com/cofe/timeline/Activity/MainActivity.java
@@ -3,17 +3,52 @@ package com.cofe.timeline.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.cofe.timeline.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView start,result;
+    private Button test;
+    private boolean canpr = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("cofe", "HAHA!");
+        init();
+    }
+
+    private void init() {
+        start = findViewById(R.id.start);
+        result = findViewById(R.id.result);
+        test = findViewById(R.id.test);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (canpr == true) {
+                    canpr = false;
+                    result.setText("不能截屏");
+                    getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+                } else if (canpr == false) {
+                    canpr = true;
+                    result.setText("可以截屏");
+                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+                }
+            }
+        });
+
     }
 }
